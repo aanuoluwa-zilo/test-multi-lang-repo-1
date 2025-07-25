58d7829cc6d4d7700bca22860d3056ddf5ed7a7e
aanuoluwa-zilo <168453615+aanuoluwa-zilo@users.noreply.github.com>
2025-07-25 16:36:41 +0100

    Add src/java/Main.java

---
+++ src/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java!");
+        
+        Calculator calc = new Calculator();
+        int result = calc.add(5, 3);
+        System.out.println("5 + 3 = " + result);
+    }
+}
